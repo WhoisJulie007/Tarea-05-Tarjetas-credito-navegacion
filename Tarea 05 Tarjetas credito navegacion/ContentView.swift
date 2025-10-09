a93a1414a9f008991c7d3f83541620aeeb43f405
Julia <maydeli.castan@iest.edu.mx>
2025-10-08 18:37:21 -0600

    Initial Commit

---
+++ Tarea 05 Tarjetas credito navegacion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tarea 05 Tarjetas credito navegacion
+//
+//  Created by WIN603 on 08/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tarea 05 Tarjetas credito navegacion/Tarea_05_Tarjetas_credito_navegacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea_05_Tarjetas_credito_navegacionApp.swift
+//  Tarea 05 Tarjetas credito navegacion
+//
+//  Created by WIN603 on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Tarea_05_Tarjetas_credito_navegacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
