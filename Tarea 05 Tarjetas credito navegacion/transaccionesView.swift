2a085d11b8a6c776f37d1a4f5b4e6f2cb2e83824
Julia <maydeli.castan@iest.edu.mx>
2025-10-22 19:04:24 -0600

    aaaaah

--- Tarea 05 Tarjetas credito navegacion/transaccionesView.swift
+++ Tarea 05 Tarjetas credito navegacion/transaccionesView.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 struct transaccionesView: View {
     var tarjetaImprimir: tarjetaModel
+    var tra: [transaccionesModel] = []
     var body: some View {
         tarjeta(tarjetaModel:  tarjetaImprimir,  altura: 220, selectedCard: .constant(1))
         Spacer()
+        transacciones(transaccion: tra[0])
+//        ForEach(tra, id: \.self) { item in
+//            transacciones(transaccion: item)
+//        }
+        
         
         Text("Estas seleccionando la tarjeta: \(tarjetaImprimir.index)")
     }
@@ -19,5 +25,6 @@ struct transaccionesView: View {
 
 #Preview {
     let x = tarjetaModel(banco: "Mercado Pago", fotobanco: "mercadopago", numero: "1234 5678 9012", nombre: "Julia Maydeli Castan ", logo: "String",colortarjeta: Color.blue, index: 1)
-    transaccionesView(tarjetaImprimir: x)
+    let x1 = transaccionesModel(cuadrito: Color.black ,icono: "train.side.front.car", texto: "Narita Airport", precio: "1200",destino: "Narita Airport", fecha: "13/10/25")
+    transaccionesView(tarjetaImprimir: x, tra: [x1])
 }
